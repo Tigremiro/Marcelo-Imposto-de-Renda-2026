ba24f5ae4d8498a0411fdaa328eee4b5bfaf6364
Tigremiro <lamoniemz@gmail.com>
2026-05-26 13:36:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-consulta').addEventListener('click', function() {
+    // Link oficial da Receita Federal para consulta
+    window.open('https://www.restituicao.receita.fazenda.gov.br/', '_blank');
+});
